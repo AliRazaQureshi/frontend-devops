1fdbd1f9a811acbefc183eacfe776ad7b5943cec
Ali Raza Qureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2024-06-06 01:10:25 +0500

    Update stop.sh

--- docker-scripts/stop.sh
+++ docker-scripts/stop.sh
@@ -1,9 +1,3 @@
 #!bin/bash
 
-if [ $( docker ps -a | grep frontend | wc -l ) -gt 0 ]; then
-  docker stop frontend
-  docker rm frontend
-  docker rmi 891377328182.dkr.ecr.us-east-1.amazonaws.com/ecr-devops-frontend:latest
-fi
-
-exit 0
+ls -l
