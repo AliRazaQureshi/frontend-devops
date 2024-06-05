06c95affb69a3e2fa0c7a8db88924dd0d53b4f37
Ali Raza Qureshi <47821624+AliRazaQureshi@users.noreply.github.com>
2024-06-06 01:10:01 +0500

    Update start.sh

--- docker-scripts/start.sh
+++ docker-scripts/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $( docker ps -a | grep frontend | wc -l ) -gt 0 ]; then
+  docker stop frontend
+  docker rm frontend
+  docker rmi 891377328182.dkr.ecr.us-east-1.amazonaws.com/ecr-devops-frontend:latest
+fi
+
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891377328182.dkr.ecr.us-east-1.amazonaws.com/ecr-devops-frontend
 
 docker pull 891377328182.dkr.ecr.us-east-1.amazonaws.com/ecr-devops-frontend:latest
